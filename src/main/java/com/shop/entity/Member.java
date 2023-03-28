0fa017910766d6843089b78ad508ac6cf1cf554e
Chanhyun Wang <rubberramen@naver.com>
2023-03-28 19:39:22 +0900

    Make Common Attribute of Entity by Using Auditing

--- src/main/java/com/shop/entity/Member.java
+++ src/main/java/com/shop/entity/Member.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @Table(name="members")
 @Getter @Setter
 @ToString
-public class Member {
+public class Member extends BaseEntity{
 
     @Id
     @Column(name="member_id")
