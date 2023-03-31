175f7500399c285d9b620d39b4829aec44a66e06
Chanhyun Wang <rubberramen@naver.com>
2023-03-31 21:02:45 +0900

    Read Order Histories and Cancel Order

--- src/main/java/com/shop/entity/OrderItem.java
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -42,5 +42,7 @@ public class OrderItem extends BaseEntity {
         return orderPrice * count;
     }
 
-
+    public void cancel() {
+        this.getItem().addStock(count);
+    }
 }
--- src/main/java/com/shop/repository/ItemImgRepository.java
+++ src/main/java/com/shop/repository/ItemImgRepository.java
@@ -5,7 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+import com.shop.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
 
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
+
+    ItemImg findByItemIdAndRepimgYn(Long itemId, String repimgYn);
+
 }
